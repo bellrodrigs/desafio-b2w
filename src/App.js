6ab2fc6cceb2de0605fd04d37d839a46db743758
Bell <isabelrodrgs@gmail.com>
2020-08-02 20:12:59 -0300

    Finishing pokestore

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import ListPokemon from  './components/ListPokemon'
 import Cart from  './components/Cart'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,13 +8,43 @@ import {useSelector} from 'react-redux'
 
 function App() {
   const pokemon = useSelector(state => state.pokemons.pokemons)
+  const [mobile, setMobile] = useState(false)
+  useEffect(() => {
+    detectMob()
+  }, []);
+
+  
+  function detectMob() {
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ];
+
+    return toMatch.some((toMatchItem) => {
+        if(navigator.userAgent.match(toMatchItem)){
+          return setMobile(true)
+        }
+    });
+}
 
   return (
     <Container fluid style={{backgroundColor:'#ededed'}}>
+      { mobile === true ?
       <Row>
-       {pokemon.length > 0 ? <Col xs={6} sm={9}><ListPokemon /></Col> : <Col xs={12} sm={12}><ListPokemon /></Col>}
-       {pokemon.length > 0 ? <Col xs={6} sm={3} style={{borderLeft: '1px #c3c3c3 solid'}}><Cart  /></Col> : null}
+        {pokemon.length > 0 ? <Col xs={12} sm={3} style={{borderBottom: '1px #c3c3c3 solid'}}><Cart  /></Col> : null}
+        {pokemon.length > 0 ? <Col xs={12} sm={9}><ListPokemon /></Col> : <Col xs={12} sm={12}><ListPokemon /></Col>}
       </Row>
+      :
+      <Row>
+        {pokemon.length > 0 ? <Col xs={12} sm={9}><ListPokemon /></Col> : <Col xs={12} sm={12}><ListPokemon /></Col>}
+        {pokemon.length > 0 ? <Col xs={12} sm={3} style={{borderLeft: '1px #c3c3c3 solid'}}><Cart  /></Col> : null}
+      </Row>
+    }
     </Container>
   );
 }
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,22 +1,20 @@
 import React, {useEffect, useState} from 'react';
 import {useSelector, useDispatch} from 'react-redux'
-import {Container, Card, Col, Row, Button} from 'react-bootstrap'
+import {Container, Card, Col, Row, Button, Modal, Image} from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faTrash,faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import {FirstLetterUpperCase} from '../helpers'
+import Ash from '../assets/images/ash.png'
 
 
 function Cart() {
   const pokemon = useSelector(state => state.pokemons.pokemons)
-  const typeName = useSelector(state => state.type)
   let length = useSelector(state => state.pokemons.pokemons.length)
   const dispatch = useDispatch()
   let totalPrice = 0
+  const [show, setShow] = useState(false);
 
-  useEffect(() => {
-    //first get    
-  }, []);
-
+  const handleShow = () => setShow(true);
   
   const calcTotal = () => {
     let total = pokemon.map(x => x.price)
@@ -28,39 +26,57 @@ function Cart() {
   }
 
   const finishPurchase = () =>{
-    alert('Sua compra foi finalizada com sucesso!')
     dispatch({type: 'CLEAN_CART_POKEMON'})
+    setShow(!show)
+    localStorage.removeItem('reduxStore')
   }
 
   return (
     <>
-    <Container fluid>
-      <h2>Carrinho</h2>
+    <Container style={{paddingTop:'15px'}} fluid>
+      <Row>
+        <Col xs={8}>
+          <h4>Carrinho</h4>
+        </Col>
+        <Col sm={3} xs={4}>
+          <h5><FontAwesomeIcon icon={faShoppingCart} /><span style={{marginLeft:'2px', border: '1px solid black', borderRadius:'3px', padding:'0px 8px 0px 8px', fontSize:'14px'}}>{length}</span></h5>
+        </Col>
+      </Row>
     {pokemon.map(x => 
-      <Card style={{marginTop:'10px', height:'90px'}}>
+      <Card style={{marginTop:'10px', widht:'120%'}} >
         <Card.Body>
           <Row>
-            <Col >
-              <Card.Img variant="top" style={{ width: '5rem', marginTop:'-20px'}} src={x.img} />
+            <Col className = "my-auto" xs={4}  sm={3}>
+              <Card.Img variant="top" style={{ width: '5rem', }} src={x.img} />
             </Col>
-            <Col sm={4}>
-            <Card.Title>{FirstLetterUpperCase(x.name)}</Card.Title>
+            <Col  className = "my-auto" xs={8}   sm={4}>
+            <Card.Title style={{fontSize:'16px'}}>{FirstLetterUpperCase(x.name)}</Card.Title>
             </Col>
-            <Col>
-              <Card.Text>Preço: R${x.price},00</Card.Text>
+            <Col className = "my-auto" xs={10}  sm={3}>
+              <Card.Text  style={{fontSize:'16px'}}>Preço: R${x.price},00</Card.Text>
             </Col>
-            <Col style={{textAlign:'right'}} sm={2}>
+            <Col className = "my-auto" xs={2}  sm={2}>
               <FontAwesomeIcon style={{color: 'red', cursor:'pointer'}} onClick={() => removePokemon(x.id)}  icon={faTrash} />
             </Col>
           </Row>
         </Card.Body>
       </Card>
     )}
-     <h2>Total: {calcTotal()}</h2>
-      {length >=1 ? <Button variant="success" block onClick={() => finishPurchase()}>Finalizar compra</Button> : null}
+     <p style={{textAlign:'right', marginTop:'10px', fontWeight:'bold'}}><span>Total:</span> <span style={{fontSize:'18px', }}>R$ {calcTotal()},00</span></p>
+      <Button style={{marginBottom:'20px'}} variant="success" block onClick={() => handleShow()}>Finalizar compra </Button>
+        <Modal centered show={show} onHide={finishPurchase}>
+          <Modal.Header closeButton>
+            <Modal.Body style={{textAlign:'center'}}>
+              <span style={{fontSize:'1.2em', fontWeight:'bold', color:'#28a745'}}>Sua compra foi finalizada com sucesso!</span>
+              <p>Dejamos boa sorte na sua jornada para se tornar um mestre Pokemon</p>
+              <Image style={{width: '25%'}} src={Ash}  />
+            </Modal.Body>
+          </Modal.Header>
+        </Modal>
     </Container>
       </>
   );
 }
 
 export default Cart;
+
